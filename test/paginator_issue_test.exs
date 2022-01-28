46ad3603859a36b925b984cd4440c783191bfbb8
Maxim I <isaev.maksim.v@gmail.com>
2022-01-28 22:04:32 +0200

    Update test

--- test/paginator_issue_test.exs
+++ test/paginator_issue_test.exs
@@ -17,10 +17,20 @@ defmodule PaginatorIssueTest do
   end
 
   test "Having an issue here" do
-    query = from(u in User, order_by: [asc: u.inserted_at, asc: u.id], select: %{user: u})
+    query = from(u in User, order_by: [asc: u.email, asc: u.id], select: %{user: u})
 
-    assert %Paginator.Page{metadata: %{after: after_cursor}} = Repo.paginate(query)
-    assert %Paginator.Page{metadata: %{after: ^after_cursor, before: ^after_cursor}} =
-             Repo.paginate(query, after: after_cursor)
+    assert %Paginator.Page{metadata: %{after: after_cursor}} =
+      Repo.paginate(query, limit: 5, cursor_fields: [email: :asc, id: :asc], fetch_cursor_value_fun: fn
+        map, key -> Map.get(map.user, key)
+      end)
+
+    assert %{email: email, id: id} = :erlang.binary_to_term(Base.decode64!(after_cursor))
+    assert is_binary(email) and is_binary(id)
+
+    assert %Paginator.Page{metadata: %{after: after_cursor, before: before_cursor}} =
+      Repo.paginate(query, after: after_cursor)
+
+    assert after_cursor == before_cursor
+    assert %{email: nil, id: nil} = :erlang.binary_to_term(Base.decode64!(after_cursor))
   end
 end
